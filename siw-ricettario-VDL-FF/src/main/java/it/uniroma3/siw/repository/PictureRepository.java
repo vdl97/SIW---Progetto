27d97cd5f77a91618f71db7262b6ebecb1e0178f
Valerio DL <v.delorenzo1@stud.uniroma3.it>
2023-07-16 18:15:50 +0200

    test

--- siw-ricettario-VDL-FF/src/main/java/it/uniroma3/siw/repository/PictureRepository.java
+++
@@ -1,9 +0,0 @@
-package it.uniroma3.siw.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import it.uniroma3.siw.model.Picture;
-
-public interface PictureRepository extends CrudRepository<Picture,Long>{
-
-}
--- siw-ricettario-VDL-FF/src/main/resources/import.sql
+++
@@ -1,6 +0,0 @@
-insert into ingrediente (id, name, category, alcohol_content) values(nextval('hibernate_sequence'), 'Bombay Sapphire', 'Gin',40);
-insert into ingrediente (id, name, category, alcohol_content) values(nextval('hibernate_sequence'), 'Campari', 'Bitter',25);
-insert into ingrediente (id, name, category, alcohol_content) values(nextval('hibernate_sequence'), 'Cinzano Rosso', 'Vermouth',158);
-insert into ingrediente (id, name, category, alcohol_content) values(nextval('hibernate_sequence'), 'Soda', 'Sodato',0);
-
-//insert into ricetta (id, title, preparationTime, dosesPerperson, descriptionRicetta
